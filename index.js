f90f8ef1d4b72b70931740898a9b82ab9b1684b3
aeolus-1 <121691@student.hillbrook.qld.edu.au>
2022-11-14 08:36:40 +1000

    textPing

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("yay")
+
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('server');
+});
+
+
+io.on('connection', async(socket) => {
+
+    socket.on('testPing', () => {
+        socket.emit("testPingResponse", {yes:true})
+       
+    })
+})
